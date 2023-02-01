1cd0a41756b7a647475fd6feb192665e72511c72
soshui <615ksh@gmail.com>
2023-02-01 17:53:35 +0900

    mainview에 header추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,10 +4,35 @@
 
 <script>
 export default {
+  components: {  },
 
 };
 </script>
 
 <style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-family: "LINESeedKR-Bd";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/LINESeedKR-Bd.woff2")
+    format("woff2");
+  font-weight: 700;
+  font-style: normal;
+}
 
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: black;
+      // color: #42b983;
+    }
+  }
+}
 </style>
