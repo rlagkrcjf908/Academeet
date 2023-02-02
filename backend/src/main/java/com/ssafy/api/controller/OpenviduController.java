17bddb47040e28a7637c9e4545b754f13753a799
Chanseok <ckstjr93@naver.com>
2023-02-02 13:22:34 +0900

    Add Log in initializeSession & createConnection

--- backend/src/main/java/com/ssafy/api/controller/OpenviduController.java
+++ backend/src/main/java/com/ssafy/api/controller/OpenviduController.java
@@ -1,6 +1,7 @@
 package com.ssafy.api.controller;
 
 import io.openvidu.java.client.*;
+import jdk.internal.org.jline.utils.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,6 +47,7 @@ public class OpenviduController {
 	@PostMapping("")
 	public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
 			throws OpenViduJavaClientException, OpenViduHttpException {
+		Log.debug("initializeSession");
 		SessionProperties properties = SessionProperties.fromJson(params).build();
 		Session session = openvidu.createSession(properties);
 		return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
@@ -60,6 +62,7 @@ public class OpenviduController {
 	public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
 			@RequestBody(required = false) Map<String, Object> params)
 			throws OpenViduJavaClientException, OpenViduHttpException {
+		Log.debug("createConnection");
 		Session session = openvidu.getActiveSession(sessionId);
 		if (session == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
