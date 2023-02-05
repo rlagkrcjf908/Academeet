16a0583b4b258d91ae240d3292a19fda88e08e9b
ckstjr93@naver.com <ckstjr93@naver.com>
2023-02-06 02:26:54 +0900

    conflit

--- frontend/src/views/MeetingView.vue
+++ frontend/src/views/MeetingView.vue
@@ -166,8 +166,8 @@ import UserVideo from "../components/UserVideo";
 //import * as faceapi from 'face-api.js';
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-const APPLICATION_SERVER_URL = "http://localhost:5000/";
-// const APPLICATION_SERVER_URL = "http://15.165.74.132:5000/";
+//const APPLICATION_SERVER_URL = "http://localhost:5000/";
+const APPLICATION_SERVER_URL = "http://15.165.74.132:5000/";
 // const APPLICATION_SERVER_URL = "http://192.168.100.88:5000/";
 // const APPLICATION_SERVER_URL = "http://192.168.0.10:5000/";
 // const APPLICATION_SERVER_URL = "https://192.168.100.88/";
