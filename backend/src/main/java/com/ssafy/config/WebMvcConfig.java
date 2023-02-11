ddaa52a2ae35f9b65189da0b34099ad6c9b61866
Chanseok <ckstjr93@naver.com>
2023-02-11 22:54:28 +0900

    [BE] filePath in addResourceHandlers

--- backend/src/main/java/com/ssafy/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/config/WebMvcConfig.java
@@ -39,6 +39,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String path = "file://" + filePath;
+        System.out.println("File Path :" + path);
         registry.addResourceHandler("/image/**")
                 .addResourceLocations(path)
                 .setCachePeriod(3600)
